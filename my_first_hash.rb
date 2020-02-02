b4f61d66b3595c4ee52fc1ce1026721b0b39ef26
Samantha Meason <sbmeason@gmail.com>
2020-02-02 23:50:17 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,9 +1,10 @@
 def my_hash = {"name" => "Samantha", "age" => "25"}
 end
-end
+
 
 
 def shipping_manifest
+  shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
